e90258ded0230f6f7a36686e549fc307a71988b7
Daniel Wasserman <wassermandaniel8@gmail.com>
2019-02-07 10:33:00 +0000

    Fix gamma calc error, add deribit option building

--- option.py
+++ option.py
@@ -1,5 +1,6 @@
 import datetime
 import math
+import pytz
 from scipy.stats import norm
 
 
@@ -9,23 +10,26 @@ day = 86400
 class Option:
     def __init__(self,
                  underlying_pair,
-                 underlying_price,
-                 call_put,
+                 option_type,
                  strike,
-                 volatility,
-                 interest_rate,
                  expiry,
-                 now=datetime.datetime.now()):
+                 interest_rate=0,
+                 volatility=None,
+                 underlying_price=None,
+                 now=datetime.datetime.now(),
+                 exchange_symbol=None):
         self.underlying_pair = underlying_pair
-        self.underlying_price = underlying_price
-        if not call_put == 'call' and not call_put == 'put':
-            raise ValueError('Expected "call" or "put", got ' + call_put)
-        self.call_put = call_put
+        if not option_type == 'call' and not option_type == 'put':
+            raise ValueError('Expected "call" or "put", got ' + option_type)
+        self.option_type = option_type
         self.strike = strike
-        self.volatility = volatility / 100
-        self.interest_rate = interest_rate
         self.expiry = expiry
+        self.interest_rate = interest_rate
+        if volatility is not None:
+            self.volatility = volatility        # decimal
+        self.underlying_price = underlying_price
         self.now = now
+        self.exchange_symbol = exchange_symbol
         self.time_left = self.get_time_left(self.now)
         self.d1 = None
         self.d2 = None
@@ -36,7 +40,7 @@ class Option:
         self.present_value = None
 
     def __str__(self):
-        return self.underlying_pair + " " + str(self.strike) + " " + self.call_put + " with expiry " + str(self.expiry)
+        return self.underlying_pair + " " + str(self.strike) + " " + self.option_type + " with expiry " + str(self.expiry)
 
     def get_time_left(self, current_datetime=datetime.datetime.now()):
         return (self.expiry - current_datetime).total_seconds() / (day * 365)
@@ -63,9 +67,10 @@ class Option:
               * time_left) / (self.volatility * math.sqrt(time_left))
         d2 = d1 - (self.volatility * math.sqrt(time_left))
         present_value = self.strike * math.exp(-self.interest_rate * time_left)
-        if self.call_put == "call":
+        theo = None
+        if self.option_type == "call":
             theo = max((self.underlying_price * norm.cdf(d1)) - (present_value * norm.cdf(d2)), 0)
-        elif self.call_put == "put":
+        elif self.option_type == "put":
             theo = max((norm.cdf(-d2) * present_value) - (norm.cdf(-d1) * self.underlying_price), 0)
         if store:
             self.d1 = d1
@@ -77,12 +82,13 @@ class Option:
     def calc_delta(self, underlying_price=None, store=True):
         if underlying_price:
             d1 = (math.log(underlying_price / self.strike) + (self.interest_rate + ((self.volatility ** 2) / 2.0))
-                       * self.time_left) / self.volatility * math.sqrt(self.time_left)
+                  * self.time_left) / (self.volatility * math.sqrt(self.time_left))
         else:
             d1 = self.d1
-        if self.call_put == 'call':
+        delta = None
+        if self.option_type == 'call':
             delta = norm.cdf(d1)
-        elif self.call_put == 'put':
+        elif self.option_type == 'put':
             delta = -norm.cdf(-d1)
         if store:
             self.delta = delta
@@ -97,6 +103,8 @@ class Option:
         return self.gamma
 
     def calc_theta(self, time_change=1/365):
+        if time_change >= self.time_left:
+            time_change = self.time_left / 2
         original_theo = self.calc_theo()
         advanced_time = self.time_left - time_change
         advanced_theo = self.calc_theo(time_left=advanced_time)
--- theoEngine.py
+++ theoEngine.py
@@ -1,44 +1,92 @@
 import datetime
+import pytz
 from option import Option
+from deribitREST import DeribitREST
 
 
 class TheoEngine:
-    def __init__(self, underlying_pair, underlying_price, strikes, expirations, atm_volatility, interest_rate=0):
+    def __init__(self, underlying_pair,
+                 underlying_price,
+                 expirations=[],
+                 strikes={},
+                 atm_volatility=None,
+                 interest_rate=0):
         self.underlying_pair = underlying_pair
         self.underlying_price = underlying_price
-        self.strikes = strikes
         self.expirations = expirations
+        self.strikes = {e: strikes for e in self.expirations}
         self.atm_volatility = atm_volatility
         self.interest_rate = interest_rate
-        self.now = datetime.datetime.now()
+        self.now = pytz.timezone('UTC').localize(datetime.datetime.now())
         self.options = {
             'call': {},
             'put': {}
         }
+        self.client = None
 
-        for expiry in self.expirations:
-            for call_put in ['call', 'put']:
-                self.options[call_put][expiry] = {}
-            for strike in self.strikes:
-                for call_put in ['call', 'put']:
-                    option = Option(
-                        underlying_pair=self.underlying_pair,
-                        underlying_price=self.underlying_price,
-                        call_put=call_put,
-                        strike=strike,
-                        volatility=self.atm_volatility,
-                        interest_rate=self.interest_rate,
-                        expiry=expiry,
-                        now=self.now
-                    )
-                    option.calc_greeks(verbose=True)
-                    self.options[call_put][expiry][strike] = option
+    def setup_client(self):
+        self.client = DeribitREST()
+
+    def build_options(self):
+        if self.strikes is not None and self.expirations is not None:
+            for expiry in self.expirations:
+                for option_type in ['call', 'put']:
+                    self.options[option_type][expiry] = {}
+                for strike in self.strikes[expiry]:
+                    for option_type in ['call', 'put']:
+                        option = Option(
+                            underlying_pair=self.underlying_pair,
+                            option_type=option_type,
+                            strike=strike,
+                            expiry=expiry,
+                            interest_rate=0,
+                            volatility=self.atm_volatility,
+                            underlying_price=self.underlying_price,
+                            now=self.now
+                        )
+                        option.calc_greeks(verbose=True)
+                        self.options[option_type][expiry][strike] = option
+
+    def build_deribit_options(self):
+        self.setup_client()
+        instruments = self.client.getinstruments()
+        options = [i for i in instruments if i['kind'] == 'option']
+        for option_info in options:
+            option_type = option_info['optionType']
+            strike = option_info['strike']
+            expiry = pytz.timezone('GMT').localize(
+                datetime.datetime.strptime(option_info['expiration'], '%Y-%m-%d %H:%M:%S GMT')
+            )
+            if expiry not in self.expirations:
+                self.expirations.append(expiry)
+            if expiry not in self.strikes:
+                self.strikes[expiry] = []
+            if strike not in self.strikes[expiry]:
+                self.strikes[expiry].append(strike)
+            option = Option(
+                underlying_pair=self.underlying_pair,
+                option_type=option_type,
+                strike=strike,
+                expiry=expiry,
+                interest_rate=0,
+                volatility=self.atm_volatility,
+                underlying_price=self.underlying_price,
+                now=self.now,
+                exchange_symbol=option_info['instrumentName']
+            )
+            print("Time left: " + str(option.time_left))
+            option.calc_greeks(verbose=True)
+            if expiry in self.options[option_type]:
+                self.options[option_type][expiry][strike] = option
+            else:
+                self.options[option_type][expiry] = {strike: option}
 
     def update_underlying_price(self, underlying_price):
         self.underlying_price = underlying_price
-        for call_put in self.options:
-            for expiry in self.options[call_put]:
-                for strike in self.options[call_put][expiry]:
-                    option = self.options[call_put][expiry][strike]
+        for option_type in self.options:
+            for expiry in self.options[option_type]:
+                for strike in self.options[option_type][expiry]:
+                    option = self.options[option_type][expiry][strike]
                     option.update_underlying_price(self.underlying_price)
                     option.calc_greeks()
+
